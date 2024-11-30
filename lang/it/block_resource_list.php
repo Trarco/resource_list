6ff7ebf7f43a1624826abad594dda255bc231710
Marco Traina <trarco22@outlook.it>
2024-11-30 17:34:01 +0100

    Add IT lang

---
+++ lang/it/block_resource_list.php
@@ -0,0 +1,35 @@
+<?php
+$string['pluginname'] = 'Elenco Risorse';
+$string['filterbyactivity'] = 'Filtra per tipo di attività';
+$string['allactivities'] = 'Tutte le attività';
+$string['title'] = 'Titolo';
+$string['generalsettings'] = 'Impostazioni generali';
+$string['descriptionsettings'] = 'Impostazioni descrizione';
+$string['description'] = 'Descrizione';
+
+// Attività
+$string['assign'] = 'Compiti';
+$string['quiz'] = 'Quiz';
+$string['forum'] = 'Forum';
+$string['resource'] = 'Risorse';
+$string['page'] = 'Pagine';
+$string['scorm'] = 'Pacchetti SCORM';
+$string['book'] = 'Libri';
+$string['chat'] = 'Chat';
+$string['choice'] = 'Scelte';
+$string['data'] = 'Database';
+$string['feedback'] = 'Feedback';
+$string['folder'] = 'Cartelle';
+$string['glossary'] = 'Glossari';
+$string['lesson'] = 'Lezioni';
+$string['lti'] = 'Strumenti esterni';
+$string['survey'] = 'Sondaggi';
+$string['url'] = 'URL';
+$string['wiki'] = 'Wiki';
+$string['workshop'] = 'Laboratori';
+$string['h5pactivity'] = 'Attività H5P'; 
+$string['imscp'] = 'Pacchetti IMS'; 
+$string['label'] = 'Etichette'; 
+
+// Raggruppa per sezioni
+$string['groupsections'] = 'Raggruppa le attività per sezioni';
