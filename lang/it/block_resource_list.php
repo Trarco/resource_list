b59bbb3cb264a228342cf15e36a91c750fa52035
Trarco <trarco22@outlook.it>
2025-07-11 14:06:49 +0200

    Missing IT string

--- lang/it/block_resource_list.php
+++ lang/it/block_resource_list.php
@@ -17,3 +17,4 @@ $string['removeindentation'] = 'Rimuovi indentazione dalle attività';
 $string['activitytitlefilter'] = 'Filtra per parola chiave';
 $string['activitytitlefilterinfo'] = 'Le attività il cui titolo contiene questa parola chiave saranno visualizzate.';
 $string['activitytitlefilters_help'] = 'Inserisci una parola chiave per visualizzare solo le attività il cui titolo la contiene. Ad esempio, per mostrare tutte le attività che contengono "Simulazione", scrivi "Simulazione". Puoi aggiungere più filtri cliccando su "Aggiungi un altro filtro". Se lasci i campi vuoti, verranno visualizzate tutte le attività.';
+$string['addmorefilters'] = 'Aggiungi un altro filtro';
